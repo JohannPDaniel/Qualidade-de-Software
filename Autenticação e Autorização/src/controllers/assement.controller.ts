1edecf6b09ed4411b9e88561a05e3cd80360b8bf
Johann Patricio Daniel <156346733+JohannPDaniel@users.noreply.github.com>
2025-01-13 21:35:21 -0300

    voltando como estava a avaliação

--- Autenticação e Autorização/src/controllers/assement.controller.ts
+++ Autenticação e Autorização/src/controllers/assement.controller.ts
@@ -5,8 +5,7 @@ import { AssessmentService } from '../service/assement.service';
 export class AssessmentController {
 	public static async create(req: Request, res: Response): Promise<void> {
 		try {
-			const { title, description, grade } = req.body;
-			const studentId = req.headers['x-student-id'] as string;
+			const { title, description, grade, studentId } = req.body;
 			const studentLogged = req.authStudent;
 
 			const data: CreateAssessmentDto = {
