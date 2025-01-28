48cf679400f50d415a6f7b2031dbce80df6c235a
Johann Patricio Daniel <156346733+JohannPDaniel@users.noreply.github.com>
2025-01-27 22:12:53 -0300

    colocando uma porta default

--- Atividade_pratica_3_Configuracao_do_Swagger_e_Documentacao_de_Rotas_da_API/src/server.ts
+++ Atividade_pratica_3_Configuracao_do_Swagger_e_Documentacao_de_Rotas_da_API/src/server.ts
@@ -7,7 +7,7 @@ import swaggerUI from "swagger-ui-express"
 import swaggerDOC from "./docs/swagger.json"
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
